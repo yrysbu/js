6b55d7b27d6455f6c1ad7f1b200468afb4d85a67
Yrysbu <yrysbu.nasirdinova.kk@gmail.com>
2023-03-06 14:29:22 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// let a = 4
+// let b = 55
+// console.log(a + b)  59
+// console.log(a * b)   220
+// console.log(a / b)  0.07272727272727272
+// console.log(a % b)  4
+
+// let a = 'hello'
+// let b = ' yrys'
+// console.log(a + b) hello yrys
+
+// let a = 0
+// let b = 'hello'
+// console.log(b / a ) NaN
+
+// let a = 5
+// let b = '5'
+// console.log(Number (b) + a) 10
+
+// let c = 2
+// let b = undefined
+// console.log( typeof c + b) numberundefined
